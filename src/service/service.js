34cc80038516498f5f9a8788c7daecd92dde3c1c
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-01-26 17:53:44 -0500

    Update TrackerService to include on and emit methods

--- src/service/service.js
+++ src/service/service.js
@@ -10,6 +10,22 @@ class TrackerService {
     Object.keys(this.trackers)
       .forEach((tracker) => this.trackers[tracker].init(this.trackers));
   }
+
+  on(eventName, callback) {
+    this._events = this._events || {};
+    this._events[eventName] = this._events[eventName] || [];
+
+    if (typeof callback === 'function') {
+      this._events[eventName].push(callback);
+    }
+  }
+
+  emit(eventName, payload) {
+    this._events = this._events || {};
+    this._events[eventName] = this._events[eventName] || [];
+
+    this._events[eventName].forEach((callback) => callback(payload));
+  }
 }
 
 module.exports = TrackerService;
