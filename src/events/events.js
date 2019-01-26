9fd5bd12fc03708fc093a03db8c41ae971528853
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-01-26 12:20:49 -0500

    Add events

---
+++ src/events/events.js
@@ -0,0 +1,8 @@
+const EVENT_A = 'event_a';
+
+const EVENT_B = 'event_b';
+
+module.exports = {
+  EVENT_A,
+  EVENT_B,
+};
---
+++ src/events/index.js
@@ -0,0 +1,5 @@
+const Events = require('./events');
+
+module.exports = {
+  Events,
+};
