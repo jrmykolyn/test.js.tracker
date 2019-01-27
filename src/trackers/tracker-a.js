f95d0414211bca9ede7a36df973f1badd6ba39e3
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-01-26 19:07:07 -0500

    Update TrackerA API: add setup methods; add augmenters

--- src/trackers/tracker-a.js
+++ src/trackers/tracker-a.js
@@ -1,10 +1,49 @@
 class TrackerA {
-  constructor(service) {
-    // TODO
+  constructor(service, opts) {
+    this.service = service;
+    this.opts = opts;
+
+    // TEMP: Expose augmentors.
+    this.augmenters = {
+      addBar: this.addBar,
+      addBaz: this.addBaz,
+    };
   }
 
   init(trackers) {
     // TODO
+  }
+
+  doSetup(data = {}) {
+    return {
+      foo: 'bar',
+      ...data,
+    };
+  }
+
+  doAugment(data) {
+    return (augmenters) => (payload) => {
+      return {
+        ...payload,
+        ...augmenters.reduce((acc, augmenter) => ({ ...acc, ...augmenter(data) }), {}),
+      }
+    };
+  }
+
+  doLink(payload) {
+    return payload;
+  }
+
+  addBar(data) {
+    return { bar: 'baz' };
+  }
+
+  addBaz(data) {
+    return { baz: 'quux' };
+  }
+
+  addQuux(data) {
+    return { quux: 'foo' };
   }
 }
 
